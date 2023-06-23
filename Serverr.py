bb2eb47aa75da63c4143c50a912eab0129975324
Surbhi Sood <79500444+surbhi-sood@users.noreply.github.com>
2023-06-23 16:10:12 +0530

    Serverr.py

--- Serverr.py
+++ Serverr.py
@@ -1,13 +1,20 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 from chatbot import get_response, get_response2
+import pyttsx3
+
+engine = pyttsx3.init()
 
 
 class ChatServer(WebSocket):
-    
+
     def handleMessage(self):
+
         # echo message back to client
         message = self.data
         # message1=self.data
+        engine.setProperty('rate', 160)
+        # engine.say(message)
+        # engine.runAndWait()
 
         response = get_response(message)
         a = get_response2(message)
@@ -15,12 +22,17 @@ class ChatServer(WebSocket):
         self.sendMessage(response)
         if a != "":
             self.sendMessage(a)
-
+        engine.say(response)
+        engine.runAndWait()
+        if a != "":
+            engine.say(a)
+            engine.runAndWait()
 
         # self.sendMessage(a)
 
     def handleConnected(self):
         print(self.address, 'connected')
+
         greeting_messages = [
             "Hi, I am Rapid!",
             "Welcome to SSFB chat server!",
@@ -29,9 +41,10 @@ class ChatServer(WebSocket):
 
         for message in greeting_messages:
             self.sendMessage(message)
-            
 
-
+        for message in greeting_messages:
+            engine.say(message)
+            engine.runAndWait()
 
     def handleClose(self):
         print(self.address, 'closed')
